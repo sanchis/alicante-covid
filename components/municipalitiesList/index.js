952be72000237a1f946580346a967a896bbee89d
sanchis <sanchis1992@gmail.com>
2021-07-18 18:52:23 +0200

    feat: update styles

--- components/municipalitiesList/index.js
+++ components/municipalitiesList/index.js
@@ -1,4 +1,6 @@
 import Card from 'components/ui/card'
+import { formatRelative, parseISO } from 'date-fns'
+import { es } from 'date-fns/locale'
 import React from 'react'
 import styles from './municipalities.module.css'
 
@@ -8,17 +10,15 @@ function getRisk (incidencia) {
     : incidencia >= 150
       ? styles.hightRisk
       : incidencia >= 50 ? styles.lowRisk : styles.noRisk
-//   return incidencia < 20
-//     ? styles.noRisk
-//     : incidencia < 50
-//       ? styles.lowRisk
-//       : incidencia < 150 ? styles.hightRisk : styles.extremeRisk
 }
 
 export default function MunicipalitiesList ({ municipalitiesData }) {
   return (
     <>
       <h2 className={styles.header}>Información sobre municipios</h2>
+      <p className='text-cursive'>Datos de los últimos 14 días</p>
+      <p className='text-cursive'>Ultima actualización: {formatRelative(parseISO(municipalitiesData.lastUpdate), new Date(), { locale: es })}</p>
+
       <div className={styles.cardContainer}>
         {municipalitiesData.municipalities.map(muni =>
           <Card key={muni.name}>
@@ -27,7 +27,6 @@ export default function MunicipalitiesList ({ municipalitiesData }) {
             <small className={`align-center ${styles.descriptionLabel}`}>Positivos</small>
             <h1 className={`align-center ${getRisk(muni.incidencia)}`}>{muni.incidencia}</h1>
             <small className={`align-center ${styles.descriptionLabel}`}>Incidencia Acumulada</small>
-            <p className='text-cursive'>Datos de los últimos 14 días</p>
           </Card>)}
       </div>
     </>

6e1d5d81acf918c07d55c47b9f90d8425b1cdf21
sanchis <sanchis1992@gmail.com>
2021-07-19 13:41:29 +0200

    feat: adapt container to desktop

--- components/header/index.js
+++ components/header/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-// import styles from './header.module.css'
+import styles from './header.module.css'
 import Image from 'next/image'
 import Logo from 'public/logo.png'
 
 export default function Header () {
   return (
-    <Image src={Logo} placeholder='blur' alt='logo' />
+    <div className={styles.imageContainer}>
+      <Image src={Logo} placeholder='blur' alt='logo' />
+    </div>
   )
 }

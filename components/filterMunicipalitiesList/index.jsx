ab53bc87f3a4ef925ea4f3898fdae78b322ead36
sanchis <sanchis1992@gmail.com>
2021-07-19 18:42:37 +0200

    feat: filter by name

--- components/filterMunicipalitiesList/index.jsx
+++ components/filterMunicipalitiesList/index.jsx
@@ -1,15 +1,16 @@
 import Card from 'components/ui/card'
 import Input from 'components/ui/input'
-import Select from 'components/ui/select'
 import React from 'react'
 import styles from './filterMunicipalities.module.css'
 
-export default function FilterMunicipalities ({ onChangeSearchText, onChangeOrderBy }) {
+export default function FilterMunicipalities ({ onChangeSearchText }) {
   return (
     <Card className={styles.containerFilter}>
-      <Select onChange={(event) => onChangeOrderBy(event.target.value)}>
-        <option>Uno</option>
-      </Select>
+      {/* <Select onChange={(event) => onChangeOrderBy(event.target.value)}>
+        <option value='name'>Alfabéticamente</option>
+        <option value='pcrPositives'>Positivos COVID</option>
+        <option value='incidencia'>Incidencia acumulada</option>
+      </Select> */}
       <Input onChange={(event) => onChangeSearchText(event.target.value)} />
 
     </Card>
--- components/municipalitiesList/index.jsx
+++ components/municipalitiesList/index.jsx
@@ -2,7 +2,7 @@ import FilterMunicipalities from 'components/filterMunicipalitiesList'
 import Card from 'components/ui/card'
 import { formatRelative, parseISO } from 'date-fns'
 import { es } from 'date-fns/locale'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './municipalities.module.css'
 
 function getRisk (incidencia) {
@@ -14,19 +14,15 @@ function getRisk (incidencia) {
 }
 
 export default function MunicipalitiesList ({ municipalitiesData }) {
+  const [searchText, setSearchText] = useState('')
   const [municipalities, setMunicipalities] = useState(municipalitiesData.municipalities)
 
-  function onChangeOrderBy (value) {
-    console.log(value)
-  }
-
-  function onChangeSearchText (value) {
-    if (value.trim() !== '') {
-      setMunicipalities(municipalitiesData.municipalities.filter(muni => muni.name.includes(value)))
-    } else {
-      setMunicipalities(municipalitiesData.municipalities)
-    }
-  }
+  useEffect(() => {
+    setMunicipalities(
+      municipalitiesData.municipalities
+        .filter(muniFilter => muniFilter.name.toLowerCase().includes(searchText))
+    )
+  }, [searchText, municipalitiesData])
 
   return (
     <>
@@ -34,16 +30,19 @@ export default function MunicipalitiesList ({ municipalitiesData }) {
       <p className='text-cursive'>Datos de los últimos 14 días</p>
       <p className='text-cursive'>Ultima actualización: {formatRelative(parseISO(municipalitiesData.lastUpdate), new Date(), { locale: es })}</p>
 
-      <FilterMunicipalities onChangeOrderBy={(val) => onChangeOrderBy(val)} onChangeSearchText={(val) => onChangeSearchText(val)} />
+      <FilterMunicipalities onChangeSearchText={(val) => setSearchText(val.trim().toLowerCase())} />
       <div className={styles.cardContainer}>
-        {municipalities.map(muni =>
-          <Card key={muni.name}>
-            <h3>{muni.name}</h3>
-            <h1 className='align-center'>{muni.pcrPositives}</h1>
-            <small className={`align-center ${styles.descriptionLabel}`}>Positivos</small>
-            <h1 className={`align-center ${getRisk(muni.incidencia)}`}>{muni.incidencia}</h1>
-            <small className={`align-center ${styles.descriptionLabel}`}>Incidencia Acumulada</small>
-          </Card>)}
+        {municipalities
+          // .filter(filterMunicipalities)
+          // .sort(sortMunicipalities)
+          .map(muni =>
+            <Card key={muni.name}>
+              <h3>{muni.name}</h3>
+              <h1 className='align-center'>{muni.pcrPositives}</h1>
+              <small className={`align-center ${styles.descriptionLabel}`}>Positivos</small>
+              <h1 className={`align-center ${getRisk(muni.incidencia)}`}>{muni.incidencia}</h1>
+              <small className={`align-center ${styles.descriptionLabel}`}>Incidencia Acumulada</small>
+            </Card>)}
       </div>
     </>
 

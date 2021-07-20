4c4edb7fed366e09039d72269c66502992ca9184
sanchis <sanchis1992@gmail.com>
2021-07-20 18:10:52 +0200

    feat: base pwa

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,14 +8,21 @@ class MyDocument extends Document {
           <title>Covid 19 Alicante | Estadísticas diarias</title>
           <meta name='description' content='Ultimas estadísticas sobre el covid en la ciudad de Alicante. Información sobre todos los municipios de la ciudad de Alicante' />
 
-          <link rel='icon' type='image/png' sizes='32x32' href='favicon-32x32.png' />
-          <link rel='icon' type='image/png' sizes='96x96' href='favicon-96x96.png' />
-          <link rel='icon' type='image/png' sizes='16x16' href='favicon-16x16.png' />
+          <link rel='icon' type='image/png' sizes='32x32' href='icon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='96x96' href='icon-96x96.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='icon-16x16.png' />
 
           <meta property='og:description' content='Ultimas estadísticas sobre el covid en la ciudad de Alicante. Información sobre todos los municipios de la ciudad de Alicante' />
           <meta property='og:image' content='/oglogo.png' />
           <meta property='og:type' content='website' />
           <meta property='og:title' content='Covid 19 Alicante | Estadísticas diarias' />
+
+          <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+          <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+          <link rel='manifest' href='/manifest.json' />
+
+          <meta name='theme-color' content='#3A5699' />
+
         </Head>
         <body>
           <Main />
